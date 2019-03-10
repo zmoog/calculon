4effd014a57854188c8e0d9496aef94a1a5943fe
Maurizio Branca <maurizio.branca@gmail.com>
2019-03-10 07:50:38 +0100

    Toggl: minor cleanup in the summary intent

--- scripts/intent/SummaryIntent.ts
+++ scripts/intent/SummaryIntent.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "inversify";
 import { ITogglService } from "../toggl/ITogglService";
 import { map, join } from "lodash";
+import { SlackMessage } from "dickbott";
 
 
 @injectable()
@@ -13,25 +14,25 @@ export class SummaryIntent {
         this.togglService = togglService;
     }
 
-    async execute(executionId: string, entities?: any): Promise<any> {
+    async execute(executionId: string, entities?: any): Promise<SlackMessage> {
 
         let today = new Date();
 
         let summary = await this.togglService.summary(today, today);
 
-        let r = map(summary.data, (s) => {
+        let blocks = map(summary.data, (s) => {
             return {
                 type: "section",
                 text: {
                     type: "mrkdwn",
-                    text: `*${s.title.project}*` + "\n * " + join(map(s.items, item => item.title.time_entry), "\n * ")
+                    text: `*${s.title.project||'Unknown'}*` + "\n * " + join(map(s.items, item => item.title.time_entry), "\n * ")
                 }
             };
         });
 
         return {
             text: "Today on Toggl",
-            blocks: r
+            blocks: blocks
         };
     }
 }
